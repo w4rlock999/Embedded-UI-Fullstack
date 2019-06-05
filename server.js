7395eac8125345b6efa517c1a869bce968869baa
Ilham Wicaksono <w4rlock999@yahoo.com>
2019-06-05 12:11:04 +0700

    posi OK lidar OK

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ var spawn = require('child_process').spawn;
 const rosnodejs = require('rosnodejs');
 const std_msgs = rosnodejs.require('std_msgs').msg;
 const ekf_nav = rosnodejs.require('sbg_driver').msg;
+const sensor_msgs = rosnodejs.require('sensor_msgs').msg;
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -24,6 +25,8 @@ var myObject = 'helloW';
 var serverState = {
     mappingRunning: false,
     runRoscore: false,
+    gpsPositionOK: false,
+    lidarDataOK: false,
     runTrajectoryLogger: false,
     runLidarMapper: false,
     withRecord: false,
@@ -42,36 +45,86 @@ let childRoscore;
 var connectedClient = 0;
 
 
-const emitMappingStatus = async socket => {
+// function ekf_nav_listener() {
+//     // Register node with ROS master
+//     rosnodejs.initNode('/listener_node')
+//       .then((rosNode) => {
+//         // Create ROS subscriber on the 'chatter' topic expecting String messages
+//         let sub = rosNode.subscribe('/ekf_nav', ekf_nav.SbgEkfNav,
+//           (data) => { // define callback execution
+//             // rosnodejs.log.info('I heard: [' + data.status.gps1_pos_used + ']');
+//             if(data.status.gps1_pos_used && !serverState.gpsPositionOK){
+//                 // socket.emit('gpsPositionOK', true);
+//                 serverState.gpsPositionOK = true;    
+//                 rosnodejs.log.info('Status gps position is True!');
+//             }else 
+//             if(!data.status.gps1_pos_used && serverState.gpsPositionOK){
+//                 // socket.emit('gpsPositionOK', false);
+//                 serverState.gpsPositionOK = false;    
+//                 rosnodejs.log.info('Status gps position is False!');
+//             }
+//           }
+//         );
+//       });
+// }
 
-    try {
-        socket.emit("ServerState", serverState.mappingRunning);
-        socket.broadcast.emit("ServerState", serverState.mappingRunning);
-        console.log("timer callback 1000ms");
-    } catch (error) {
-        console.error(`Error: ${error.code}`);
-    }
-    // res.send({ express: 'hello from backend!' });
-
-};
-
-function listener() {
+function ros_topics_listener() {
     // Register node with ROS master
-    rosnodejs.initNode('/listener_node')
+    rosnodejs.initNode('/server_listener_node')
       .then((rosNode) => {
-        // Create ROS subscriber on the 'chatter' topic expecting String messages
-        let sub = rosNode.subscribe('/ekf_nav', ekf_nav.SbgEkfNav,
-          (data) => { // define callback execution
-            rosnodejs.log.info('I heard: [' + data.position.x + ']');
+        
+        let ekf_nav_sub = rosNode.subscribe('/ekf_nav', ekf_nav.SbgEkfNav,
+          (data) => { 
+        
+            if(data.status.gps1_pos_used && !serverState.gpsPositionOK){
+                serverState.gpsPositionOK = true;    
+                rosnodejs.log.info('Status gps position is True!');
+            }else 
+            if(!data.status.gps1_pos_used && serverState.gpsPositionOK){
+                serverState.gpsPositionOK = false;    
+                rosnodejs.log.info('Status gps position is False!');
+            }
+          }
+        );
+
+        let velodyne_points_sub = rosNode.subscribe('/velodyne_points', sensor_msgs.PointCloud2,
+          (data) => {  
+            if(!serverState.lidarDataOK){
+                serverState.lidarDataOK = true;
+                rosnodejs.log.info('lidar data OK!');
+            }    
           }
         );
       });
 }
 
+
 if (require.main === module) {
     // Invoke Main Listener Function
-    listener();
+    // ekf_nav_listener();
+    ros_topics_listener();
 }
+
+const emitMappingStatus = async socket => {
+
+    try {
+        socket.emit("ServerState", serverState.mappingRunning);
+        socket.broadcast.emit("ServerState", serverState.mappingRunning);
+        
+        socket.emit("gpsPositionOK",serverState.gpsPositionOK);
+        socket.broadcast.emit("gpsPositionOK",serverState.gpsPositionOK);
+        
+        socket.emit("lidarDataOK", serverState.lidarDataOK);
+        socket.broadcast.emit("lidarDataOK", serverState.lidarDataOK);
+
+
+
+        console.log("timer callback 1000ms");
+    } catch (error) {
+        console.error(`Error: ${error.code}`);
+    }
+    // res.send({ express: 'hello from backend!' });
+};
 
 io.on("connection", socket => {
     console.log("New client connected");
